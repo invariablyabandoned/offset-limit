adc25bda4679bbecf7d12d284db5d56764d541d0
Daniel Emery <danielremery@gmail.com>
2024-05-07 07:19:35 +0000

    Remove redundant explain in performance scenarios

--- sql/4_large_performance_scenarios.sql
+++ sql/4_large_performance_scenarios.sql
@@ -19,11 +19,3 @@ from large_sample_dataset
 order by created_at ASC
 offset 99900000
 limit 1000;
-
--- Explain
-explain
-select *
-from large_sample_dataset
-order by created_at ASC
-offset 99900000
-limit 1000;
